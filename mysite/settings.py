a772206c6e0b7cf8aa71a0ca4dd3de7e4b031076
Carlton Agesa Mbaja <65759360+carlagesa@users.noreply.github.com>
2023-03-18 01:43:24 -0700

    Updated allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    'https://plotihubs.vercel.app/',
 ]
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
