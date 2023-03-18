689cf8cead7099616cfcac0276a93d146d0a6aa8
Carlton Agesa Mbaja <65759360+carlagesa@users.noreply.github.com>
2023-03-18 19:24:40 +0300

    Reverted allowed host to "*"

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://plotihub.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     'https://plotihubs-v2.vercel.app',
 ]
