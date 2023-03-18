7ad792038488f45c6e50092db8f1c1d22992f5b0
Carlton Agesa Mbaja <65759360+carlagesa@users.noreply.github.com>
2023-03-18 19:04:32 +0300

    Modified allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://plotihubs.vercel.app/']
 CORS_ALLOWED_ORIGINS = [
     'https://plotihubs.vercel.app',
 ]
