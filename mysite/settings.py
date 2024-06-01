a04d4331f8568d69e8abffa9e68ff8476ee28bc5
carlton <carltonagesa@gmail.com>
2024-06-01 08:10:44 +0300

    Add dj-database-url

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
